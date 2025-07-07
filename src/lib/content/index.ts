6efee089f27ad6e78afa58cc4b1c3cf246cbead7
Uncle Common <krishnerkar@gmail.com>
2025-07-07 10:21:32 +0000

    Update brand identity variables

--- src/lib/content/index.ts
+++ src/lib/content/index.ts
@@ -3,5 +3,5 @@ export const CONFIG = {
 	description: "A platform that sifts through job listings to match job seekers with relevant opportunities, providing concise insights into companies and roles, and enabling applicants to create profiles optimized for employers.",
 	image: "/og.png",
 	url: "https://grids.com",
-	fontFamily: "Inter Variable"
+	fontFamily: "DM Sans"
 };
